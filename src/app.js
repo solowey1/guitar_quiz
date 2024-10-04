ac3df9682964507c7a1c9cc7ea1822fd465ba760
solowey <solowey1@gmail.com>
2024-10-04 18:05:54 +0300

    upd / route

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 require('dotenv').config();
+import packageJson from '../package.json';
 const express = require('express');
 const mongoose = require('mongoose');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use(express.json());
 
+app.use('/', (req, res) => {
+  res.json({
+    message: 'Добро пожаловать в Guitar Quiz API',
+    version: packageJson.version,
+    endpoints: [
+      '/api/questions',
+      '/api/users',
+      '/api/user-answers'
+    ]
+  });
+});
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/questions', require('./routes/questionRoutes'));
 app.use('/api/user-answers', require('./routes/userAnswerRoutes'));
