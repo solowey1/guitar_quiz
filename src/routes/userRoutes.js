e5eb31686c3c238f5cbe5f3dd15c6decde7f0d2f
solowey <solowey1@gmail.com>
2024-10-05 02:43:38 +0300

    upd

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,13 +4,13 @@ const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/')
-  .get(protect, controller.getUsers)
-  .post(controller.createUser);
-
 router.route('/:id')
   .get(controller.getUser)
   .put(controller.updateUser)
   .delete(protect, controller.deleteUser);
+  
+router.route('/')
+  .get(protect, controller.getUsers)
+  .post(controller.createUser);
 
 module.exports = router;
