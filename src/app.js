e5e4328d19e6b48270c8a90306ffa7e9d9a3dbe2
solowey <solowey1@gmail.com>
2024-10-05 02:14:07 +0300

    add cors

--- src/app.js
+++ src/app.js
@@ -3,26 +3,20 @@ const packageJson = require('../package.json');
 const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
+const corsMiddleware = require('./middleware/corsConfig');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-// Добавление morgan для логирования HTTP запросов
+app.use(corsMiddleware);
+
 app.use(morgan('[:date[iso]] :method :url :status :response-time ms - :res[content-length]'));
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Successfully connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use(express.json());
-
-// Middleware для логирования всех запросов
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  console.log('Headers:', req.headers);
-  console.log('Body:', req.body);
-  next();
-});
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/questions', require('./routes/questionRoutes'));
---
+++ src/middleware/corsConfig.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://orion-quiz.webflow.io',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true
+};
+
+module.exports = cors(corsOptions);
