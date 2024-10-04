b4bf1684d07a2cd696fe5f61b79e9f231187cfc8
solowey <solowey1@gmail.com>
2024-10-04 18:31:31 +0300

    upd

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -11,16 +11,13 @@ const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/')
-  .get(getQuestions)
-  .post(protect, createQuestion);
+router.route('/').get(getQuestions)
+router.route('/').post(protect, createQuestion);
 
-router.route('/:id')
-  .get(getQuestion)
-  .put(protect, updateQuestion)
-  .delete(protect, deleteQuestion);
+router.route('/:id').get(getQuestion)
+router.route('/:id').put(protect, updateQuestion)
+router.route('/:id').delete(protect, deleteQuestion);
 
-router.route('/number/:number')
-  .get(getQuestionByNumber);
+router.route('/number/:number').get(getQuestionByNumber);
 
 module.exports = router;
