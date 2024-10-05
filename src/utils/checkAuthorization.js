a65afbd19f4e5874efbd1487be01cdcfa0efa7b8
solowey <solowey1@gmail.com>
2024-10-05 03:31:27 +0300

    upd authCheck

--- src/utils/checkAuthorization.js
+++ src/utils/checkAuthorization.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 
 exports.checkAuthorization = (req) => {
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader.startsWith('Bearer ')) {
     const token = authHeader.split(' ')[1];
     try {
-      jwt.verify(token, process.env.JWT_SECRET);
-      return true;
+      return token === process.env.JWT_SECRET;
     } catch (error) {
       console.error('Invalid token:', error.message);
     }
