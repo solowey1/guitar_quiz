0eb0952571261c7b23a080f9775359a628c5f893
solowey <solowey1@gmail.com>
2024-10-06 20:49:12 +0300

    upd

--- src/controllers/leaderboardController.js
+++ src/controllers/leaderboardController.js
@@ -5,25 +5,32 @@ const { successResponse, errorResponse } = require('../utils/responseHandler');
 
 const getLeaderboardData = async () => {
   const leaderboard = await Answer.aggregate([
-    { $group: {
-      _id: "$userId",
-      totalScore: { $sum: "$score" },
-      totalTime: { $sum: "$timeSpent" }
-    }},
+    {
+      $group: {
+        _id: "$userId",
+        totalScore: { $sum: "$score" },
+        totalTime: { $sum: "$timeSpent" }
+      }
+    },
     { $sort: { totalScore: -1, totalTime: 1 } },
-    { $lookup: {
-      from: "users",
-      localField: "_id",
-      foreignField: "_id",
-      as: "user"
-    }},
+    {
+      $lookup: {
+        from: "users",
+        localField: "_id",
+        foreignField: "_id",
+        as: "user"
+      }
+    },
     { $unwind: "$user" },
-    { $project: {
-      _id: 1,
-      name: { $concat: ["$user.firstname", " ", "$user.lastname"] },
-      totalScore: 1,
-      totalTime: 1
-    }}
+    {
+      $project: {
+        _id: 0,
+        userId: "$_id",
+        name: { $concat: ["$user.firstname", " ", "$user.lastname"] },
+        totalScore: 1,
+        totalTime: 1
+      }
+    }
   ]);
 
   return leaderboard.map((entry, index) => ({
@@ -57,108 +64,3 @@ exports.getLeaderboard = asyncHandler(async (req, res) => {
     return errorResponse(res, error.message);
   }
 });
-
-
-
-// const mongoose = require('mongoose');
-// const Answer = require('../models/Answer');
-// const User = require('../models/User');
-// const asyncHandler = require('../utils/asyncHandler');
-// const { successResponse, errorResponse } = require('../utils/responseHandler');
-
-// const getLeaderboardData = async (limit = 5) => {
-//   const leaderboard = await Answer.aggregate([
-//     {
-//       $group: {
-//         _id: '$userId',
-//         totalScore: { $sum: '$score' },
-//         totalTime: { $sum: '$timeSpent' }
-//       }
-//     },
-//     { $sort: { totalScore: -1, totalTime: 1 } },
-//     { $limit: limit },
-//     {
-//       $lookup: {
-//         from: 'users',
-//         localField: '_id',
-//         foreignField: '_id',
-//         as: 'userDetails'
-//       }
-//     },
-//     { $unwind: '$userDetails' },
-//     {
-//       $project: {
-//         _id: 1,
-//         totalScore: 1,
-//         totalTime: 1,
-//         firstname: '$userDetails.firstname',
-//         lastname: '$userDetails.lastname'
-//       }
-//     }
-//   ]);
-
-//   return leaderboard.map((entry, index) => ({
-//     position: index + 1,
-//     name: `${entry.firstname} ${entry.lastname}`,
-//     score: entry.totalScore,
-//     time: entry.totalTime,
-//     userId: entry._id
-//   }));
-// };
-
-// exports.getLeaderboard = asyncHandler(async (req, res) => {
-//   try {
-//     const userId = req.query.userId;
-//     const leaderboard = await getLeaderboardData();
-
-//     if (!userId) {
-//       return successResponse(res, leaderboard.slice(0, 5));
-//     }
-
-//     const userPosition = await Answer.aggregate([
-//       {
-//         $group: {
-//           _id: '$userId',
-//           totalScore: { $sum: '$score' },
-//           totalTime: { $sum: '$timeSpent' }
-//         }
-//       },
-//       { $sort: { totalScore: -1, totalTime: 1 } },
-//       {
-//         $group: {
-//           _id: null,
-//           positions: { $push: '$_id' }
-//         }
-//       },
-//       {
-//         $project: {
-//           position: { $indexOfArray: ['$positions', mongoose.Types.ObjectId(userId)] }
-//         }
-//       }
-//     ]);
-
-//     const userRank = userPosition[0] ? userPosition[0].position + 1 : null;
-
-//     if (userRank === null) {
-//       return errorResponse(res, 'User not found in leaderboard', 404);
-//     }
-
-//     const userDetails = leaderboard.find(entry => entry.userId.toString() === userId);
-
-//     if (!userDetails) {
-//       return errorResponse(res, 'User details not found', 404);
-//     }
-
-//     const response = {
-//       topFive: leaderboard.slice(0, 5),
-//       userInfo: {
-//         ...userDetails,
-//         position: userRank
-//       }
-//     };
-
-//     return successResponse(res, response);
-//   } catch (error) {
-//     return errorResponse(res, error.message);
-//   }
-// });
