27c16eac7892b397c8807ef54cd8c4acbeb31a8c
solowey <solowey1@gmail.com>
2024-12-06 20:40:51 +0300

    upd model

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  lastname: { type: String, required: true },
-  firstname: { type: String, required: true },
+  email: { type: String },
+  phone: { type: String },
+  lastname: { type: String },
+  firstname: { type: String },
   patronymic: { type: String },
   workplace: { type: String },
   position: { type: String }
